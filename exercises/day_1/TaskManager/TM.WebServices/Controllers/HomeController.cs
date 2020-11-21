74e28a79d29c2d5d5d2b98958bdb93f447e1820d
Pavel Kyurkchiev <pavkyu@gmail.com>
2020-11-21 10:13:14 +0200

    first part of the day

---
+++ exercises/day_1/TaskManager/TM.WebServices/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TM.WebServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world!");
+        }
+
+        [HttpGet("[action]/{id}")]
+        public IActionResult CombineParams([FromRoute] int id, [FromQuery] string name)
+        {
+            string result = "";
+
+            for (int i = 0; i < id; i++)
+            {
+                result += $"Hello {name}!\n";
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult CombineQueryParams([FromQuery] int id, [FromQuery] string name)
+        {
+            string result = "";
+
+            for (int i = 0; i < id; i++)
+            {
+                result += $"Hello {name}!\n";
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("[action]/{id}/{name}")]
+        public IActionResult CombineRouteParams([FromRoute] int id, [FromRoute] string name)
+        {
+            string result = "";
+
+            for (int i = 0; i < id; i++)
+            {
+                result += $"Hello {name}!\n";
+            }
+
+            return Ok(result);
+        }
+    }
+}
