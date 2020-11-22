8a7a0f1babf7bc5c057723cd5003fa00c6b78062
Pavel Kyurkchiev <pavkyu@gmail.com>
2020-11-22 09:59:14 +0200

    day 2

---
+++ exercises/day_2/AssignmentManager/AM.Data/DbContexts/AssignmentManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AM.Data.Entities;
+
+namespace AM.Data.DbContexts
+{
+    public class AssignmentManagerDbContext : DbContext
+    {
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public AssignmentManagerDbContext(DbContextOptions<AssignmentManagerDbContext> options) 
+            : base(options) { }
+    }
+}
