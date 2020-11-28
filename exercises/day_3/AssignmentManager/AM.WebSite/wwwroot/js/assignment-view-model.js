54856e724945755ce4cec7b0b6a60697b87d8bba
Pavel Kyurkchiev <pavkyu@gmail.com>
2020-11-28 10:42:02 +0200

    assignment view model

---
+++ exercises/day_3/AssignmentManager/AM.WebSite/wwwroot/js/assignment-view-model.js
@@ -0,0 +1,29 @@
+﻿var AssignmentViewModel = function () {
+    let self = this;
+    let baseUrl = "http://localhost:55814/api/assignments";
+
+    self.assignments = ko.observableArray();
+    self.error = ko.observable();
+
+    function ajaxHelper(url, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: url,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function () { self.error('Ajax request error'); });
+    };
+
+    function getAllAssignments() {
+        ajaxHelper(baseUrl, 'GET').done(function (data) {
+            self.assignments(data.assignments);
+            console.log(data.assignments);
+        });
+    };
+
+    getAllAssignments();
+};
+
+ko.applyBindings(new AssignmentViewModel);
