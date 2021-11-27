cc5dee63f0483bd1405cc6d636e1496e26348002
Pavel Kyurkchiev <pavkyu@gmail.com>
2021-11-27 12:06:44 +0200

    day 3

---
+++ exercises/day_3/AssignmentManager/AM.ApplicationServices/Messaging/Assignments/InsertAssignmentRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationServices.Messaging.Assignments
+{
+    public class InsertAssignmentRequest : ServiceRequestBase
+    {
+        public InsertAssignmentRequest(AssignmentPropertiesViewModel properties) 
+        {
+            AssignmentProperties = properties;
+        }
+
+        public AssignmentPropertiesViewModel AssignmentProperties { get; set; }
+    }
+}
---
+++ exercises/day_3/AssignmentManager/AM.Repositories/Interfaces/IAssignmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using AM.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Repositories.Interfaces
+{
+    public interface IAssignmentRepository : IRepository<Assignment>
+    {
+    }
+}
